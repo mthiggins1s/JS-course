79f83a834c98217045b53f37145ffa451455e51e
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-27 15:30:36 -0600

    ended at timestamp 4:24:48 in JS video

---
+++ lesson-8/scripts/notes.js
@@ -0,0 +1,16 @@
+// What is an Object?
+    // an object groups multiple values together.
+
+// Example
+    // const product = { // this is an object.
+   // name: 'socks',
+   // price: 1090,
+// };
+// console.log(product); // Output: {name: 'socks', price: 1090}
+
+    // Syntax for Objects
+        // the name and price are the 'property'.
+        // the value associated with the property is a 'property-value' pair.
+    // to create an object, we start with a open curley bracket and close with a closing curly bracket.
+
+// an Object is just another type of value, we can save an object inside a variable and we can also console.log an object.
