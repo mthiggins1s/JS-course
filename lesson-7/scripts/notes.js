e3f46206bed4395f2f5e86e7f0073fa4c42d094a
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-27 14:59:56 -0600

    completed lesson-7-functions and cleaned up and simplified rock-paper-scissors-07

--- lesson-7/scripts/notes.js
+++ lesson-7/scripts/notes.js
@@ -13,3 +13,27 @@
     // inside the curly brackets, we put the code that we want to run when calling the function.
 
     // IMPORTANT: the function will ONLY RUN when its called. Ex. calling function1(); will run the content inside it.
+
+// Return Statement:
+    // allows us to get a value out of a function.
+
+// Syntax rules for a Return Statement:
+    // just type 'return' and a value. Example: return 'rock'; or return variable1, etc.
+        // returning a value from a function, or the 'return value'.
+        // We don't always have to return a value, just calling return; will result in 'undefined.
+    // When we use a return statement, it ends the function IMMEDIATELY.
+
+
+// Parameters; puts a value INTO a function; opposite to a return statement.
+    // works the same way as a variable.
+    // Example:
+        // function calculateTax(parameter1) {
+           // console.log(parameter1 * 0.10) // 10.00 into cents.
+       // }
+       // calculateTax(2000); // runs the function // Output: 100 cents
+
+// When a function has a parameter, we usually say this function TAKES a parameter. If this paramenter should be a number, it should be this function takes a number. When we put a value into the function, its called PASSING the value into a function.
+
+// functions can have MORE THAN ONE PARAMETER.
+
+// we can put an if-statement inside another if-statement.
