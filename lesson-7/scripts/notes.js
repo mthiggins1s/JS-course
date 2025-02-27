fb568b3759877038c581a931cdfae3e28bb117cd
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-27 13:52:09 -0600

    cleaned up rock-paper-scissors with functions

---
+++ lesson-7/scripts/notes.js
@@ -0,0 +1,15 @@
+// What is a function?
+    // a function lets us reuse code.
+    // makes our code cleaner by removing duplicated code.
+        // if we wanted to run the code again, we just type the function name again with brackets. Example:
+            // function1();
+            // function1();
+                // this will run the code twice.
+
+// Syntax Rules for Functions
+    // function -> creates a new function
+        // then we choose a name (cannot use the word 'function', no starting with numbers or special characters.)
+    // use camelCase
+    // inside the curly brackets, we put the code that we want to run when calling the function.
+
+    // IMPORTANT: the function will ONLY RUN when its called. Ex. calling function1(); will run the content inside it.
