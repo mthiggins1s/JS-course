d2a12f0ce60ceeda3146529dcfe81424378a5452
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-27 10:46:21 -0600

    created rock paper scissors and started lesson 6

---
+++ lesson-6/scripts/notes.js
@@ -0,0 +1,24 @@
+// What are Booleans?
+    // Booleans are another type of value;
+        // Two values: True and False
+
+// What is the purpose of Boolean values?
+    // a Boolean value represents whether something is True or False.
+
+// We do NOT surround booleans with quotes; because it is not a string.
+    // to check for a boolean value, we can use 'typeof'. Ex. console.log(typeof true); output: boolean
+
+
+
+// Comparison Operators:
+
+    //  > Greater Than
+    //  < Less Than
+    //  >= Greater Than or Equal to
+    //  <= Less Than or Equal to
+    //  === Equal to (== and ===; the double equal tries to convert both values to the same type. the triple equal will look strictly for the same two values). Important: the same rules apply for != and !==.
+    //  !== Not Equal to
+
+// If-Statements
+    // lets us write multiple groups of code
+    // and then decide which code to run
